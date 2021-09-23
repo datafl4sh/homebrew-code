e61fed6febde4f441211a8b357ae516d61714868
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2021-09-23 15:36:36 +0200

    Updated SILO formula.

--- silo.rb
+++ silo.rb
@@ -1,8 +1,10 @@
 class Silo < Formula
   desc "LLNL mesh and field Silo I/O library. Allows creating databases for VisIt."
   homepage "https://wci.llnl.gov/simulation/computer-codes/silo"
-  url "https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2-bsd.tar.gz"
-  sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
+  #url "https://wci.llnl.gov/sites/wci/files/2021-01/silo-4.10.2-bsd.tgz"
+  #sha256 "4b901dfc1eb4656e83419a6fde15a2f6c6a31df84edfad7f1dc296e01b20140e"
+  url "https://wci.llnl.gov/sites/wci/files/2021-01/silo-4.10.2.tgz"
+  sha256 "3af87e5f0608a69849c00eb7c73b11f8422fa36903dd14610584506e7f68e638"
   revision 4
 
 #  bottle do
@@ -13,18 +15,19 @@ class Silo < Formula
 #  end
 
   option "with-static", "Build as static instead of dynamic library"
-  option "without-lite-headers", "Do not install PDB lite headers"
+  #option "without-lite-headers", "Do not install PDB lite headers"
 
-  depends_on :x11 => :optional
+  #depends_on :x11 => :optional
   #depends_on gcc
   depends_on "readline"
   depends_on "hdf5" => :recommended
+  depends_on "gfortran"
 
   def install
     args = ["--prefix=#{prefix}"]
     args << "--enable-optimization"
     args << "--with-zlib"
-    args << "--enable-install-lite-headers" if build.with? "lite-headers"
+    #args << "--enable-install-lite-headers" if build.with? "lite-headers"
     args << "--enable-shared" if build.without? "static"
     args << "--enable-x" if build.with? "x11"
     args << "--with-hdf5=#{Formula["hdf5"].opt_prefix}" if build.with? "hdf5"
@@ -38,6 +41,8 @@ class Silo < Formula
       rm lib/"libsilo.settings"
     end
   end
+
+  patch :DATA
 
   test do
     (testpath/"test.c").write <<-EOS.undent
@@ -67,3 +72,22 @@ class Silo < Formula
     system "./test"
   end
 end
+__END__
+diff --git a/src/hdf5_drv/silo_hdf5.c b/src/hdf5_drv/silo_hdf5.c
+index 6fd99ec..aa703a4 100644
+--- a/src/hdf5_drv/silo_hdf5.c
++++ b/src/hdf5_drv/silo_hdf5.c
+@@ -15776,11 +15776,11 @@ db_hdf5_SortObjectsByOffset(DBfile *_dbfile, int nobjs,
+             H5O_info_t oinfo;
+             hid_t oid;
+             if ((oid=H5Oopen(dbfile->cwg, names[i], H5P_DEFAULT))<0 ||
+-                 H5Oget_info(oid, &oinfo)<0 ||
++                 H5Oget_info1(oid, &oinfo)<0 ||
+                  H5Oclose(oid)<0)
+                 iop[i].offset = HADDR_MAX;
+-            else
+-                iop[i].offset = oinfo.addr;
++            //else
++            //    iop[i].offset = oinfo.addr;
+         }
+     }
